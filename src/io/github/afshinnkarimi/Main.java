10bf3b1f29a1381e3a77cb16da1f1fe12084e72f
karimiafshin <afshinnkarimi@gmail.com>
2025-05-29 16:30:42 +0330

    `Refactor input validation for principal and annual interest rate in Main.java`

--- src/io/github/afshinnkarimi/Main.java
+++ src/io/github/afshinnkarimi/Main.java
@@ -12,13 +12,27 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000) {
+                break; // Valid input, exit the loop
+            }
+            System.out.println("Enter a number between 1,000 and 1,000,000.");   
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInerest = scanner.nextFloat();
-        float monthlyInterest = annualInerest / PERCENT / MONTHS_IN_YEAR;
+        float annualInterest = 0;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
+                break; // Valid input, exit the loop
+            }
+                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+        }
 
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         System.out.print("Period (Years): ");
         byte years = scanner.nextByte();
         int numberOfPayments = years * MONTHS_IN_YEAR;
@@ -27,8 +41,9 @@ public class Main {
                     * Math.pow(1 + monthlyInterest, numberOfPayments)
                     / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
-        String mortgateFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgateFormatted);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
 
+        scanner.close(); // Close the scanner
     }
 }
